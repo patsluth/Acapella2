a036ec9eba6a47abb241afca4dcd35b6a5ac6a3a
patsluth <pat.sluth@gmail.com>
2014-11-26 11:12:06 -0700

    fixed libsw warnings

---
+++ acapella/.theos/_/Library/Frameworks/AcapellaKit.framework/Headers/AcapellaKit.h
@@ -0,0 +1,31 @@
+//
+//  AcapellaKit.h
+//  AcapellaKit
+//
+//  Created by Pat Sluth on 2014-10-26.
+//  Copyright (c) 2014 Pat Sluth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+
+
+FOUNDATION_EXPORT double AcapellaKitVersionNumber; //! Project version number for AcapellaKit.
+FOUNDATION_EXPORT const unsigned char AcapellaKitVersionString[]; //! Project version string for AcapellaKit.
+
+
+
+
+
+#import <AcapellaKit/SWAcapellaBase.h>
+#import <AcapellaKit/SWAcapellaScrollViewProtocol.h>
+#import <AcapellaKit/SWAcapellaTableView.h>
+#import <AcapellaKit/SWAcapellaScrollView.h>
+#import <AcapellaKit/SWAcapellaActionIndicator.h>
+#import <AcapellaKit/SWAcapellaActionIndicatorController.h>
+
+
+
+
---
+++ acapella/.theos/_/Library/Frameworks/AcapellaKit.framework/Headers/SWAcapellaScrollView.h
@@ -0,0 +1,18 @@
+//
+//  SWAcapellaScrollView.h
+//  AcapellaKit
+//
+//  Created by Pat Sluth on 2014-11-08.
+//  Copyright (c) 2014 Pat Sluth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SWAcapellaScrollViewProtocol.h"
+
+@interface SWAcapellaScrollView : UIScrollView <SWAcapellaScrollViewProtocol>
+
+@end
+
+
+
+
